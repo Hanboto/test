96ef0bec1e05ee7decf3387c4b226c073e2376a0
Hanboto <muratbekov.hanboto@gmail.com>
2019-02-19 20:18:01 +0600

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    ;
+
+    public static void main(String[] args) {
+        final String variable1 = "Hello ";
+        String variable2 = "World";
+        String variable3 = variable1 + variable2;
+        System.out.println(variable3);
+        System.out.println(variable1);
+
+        int f = 2;
+        int h = 3;
+        int d = f + h;
+        System.out.println(d);
+        //git add .
+        //git status
+
+    }
+}
